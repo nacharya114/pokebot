162727f7c1f2eba73a23a136ecfb53c5dda2e7a8
Leo <leo@Leos-MBP-2.hsd1.mi.comcast.net>
2020-07-05 18:23:00 -0400

    implemented method to extract pokemon from database and obtain stats

--- pokebot/models/estimator/statFormulas.py
+++ pokebot/models/estimator/statFormulas.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.random import rand
-
+import pandas as pd
 import poke_env as penv
 from poke_env.environment.battle import Battle
 from poke_env.environment.pokemon import Pokemon
@@ -20,3 +20,31 @@ def calcStat(pokemon,baseStat, IV,EV, natureMod):
 
 	stat = np.floor(  natureMod * (5 + np.floor(   ((2 * baseStat + IV + np.floor(EV/4)  ) * pokemon.level) / (100))   ) )
 	return stat
+
+
+
+def extractBaseStats(pokemon, database):
+
+	
+	# print(df.head(5))
+	pokemonEntry = df.loc[df['name'] == pokemon]
+	print(pokemonEntry)
+	# print(pokemonEntry['hp'].iloc[0])
+	# print(pokemonEntry['attack'].iloc[0])
+	stats = [pokemonEntry['hp'].iloc[0],\
+		pokemonEntry['attack'].iloc[0],\
+		pokemonEntry['defense'].iloc[0],\
+		pokemonEntry['sp_attack'].iloc[0],\
+		pokemonEntry['sp_defense'].iloc[0],\
+		pokemonEntry['speed'].iloc[0]]
+	print(stats)
+	return stats
+
+
+def loadDatabase(filepath = '../../../resources/pokemon.csv'):
+	df = pd.read_csv(filepath, usecols = ['name','hp','attack','defense','sp_attack','sp_defense','speed'])
+	return df
+
+if __name__ == '__main__':
+	df = loadDatabase(filepath = '../../../resources/pokemon.csv')
+	extractBaseStats('Excadrill', df)
